14d42fb407766b8e34c42e366283710d3c933fbe
Samkit Jain <samkitchoudhary@gmail.com>
2022-04-18 18:48:01 +0530

    before netlify first commit

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Alert alert={alert} />
           <div className="container">
             <Switch>
-              <Route exact path="/">
+              <Route path="/">
                 <Home />
               </Route>
               <Route exact path="/about">
